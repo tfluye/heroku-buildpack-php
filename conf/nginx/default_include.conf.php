e71c35c11afa6914a465dddb1a68dc7c3efc0857
LU YE <lu@tomorrowfinance.com.au>
2017-06-05 10:18:00 +1000

    Update default_include.conf.php

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,6 +1,3 @@
-location / {
-    index  index.php index.html index.htm;
-}
 
 # for people with app root as doc root, restrict access to a few things
 location ~ ^/(composer\.|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
